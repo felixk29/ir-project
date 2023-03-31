f36b519198353687a6d3ab8bd08660d3c8383f5e
sqbly <kskublicki@gmail.com>
2023-03-31 19:39:24 +0200

    Borda and Copeland (untested)

--- voting_rules.py
+++ voting_rules.py
@@ -1,24 +1,89 @@
 # Module with voting functions
 
-from abc import ABC, ABCMeta, abstractmethod
-from typing import Any, TypeVar 
-
-class Comparable(metaclass=ABCMeta):
-    @abstractmethod
-    def __lt__(self, other: Any) -> bool: ...
-
-RankingEntry = TypeVar('RankingEntry', bound=Comparable)
+from abc import ABC, abstractmethod
+from typing import Any 
 
 class RankingEnsemble(ABC):
 
-    @abstractmethod
     @staticmethod
-    def combine(rankings: list[list[RankingEntry]]) -> list[RankingEntry]: ...
+    @abstractmethod
+    def combine(rankings: list[list[Any]]) -> list[Any]: ...
 
 class Borda(RankingEnsemble):
     def combine(rankings): 
-        pass
+        item_scores = {}
+        for ranking in rankings:
+            modifier = len(ranking) - 1
+            for item in ranking:
+                current_score = item_scores.get(item, 0)
+                current_score += modifier
+                item_scores[item] = current_score
+                modifier -= 1
+
+        sorted_scores = sorted(item_scores.items(), key=lambda x:x[1], reverse=True)
+
+        final_ranking = [item for item, _ in sorted_scores]
+
+        return final_ranking[:len(rankings[0])]
 
 class Copeland(RankingEnsemble):
     def combine(rankings):
-        pass
+        INF = 1e9
+
+        ranking_positions = []
+
+        items_set = set()
+        for ranking in rankings:
+            for item in ranking:
+                items_set.add(item)
+            
+        items = list(items_set)
+
+        item_number = len(items)
+
+        for ranking in rankings:
+            current_dict = {}
+            for i in range(len(ranking)):
+                item = ranking[i]
+                current_dict[item] = i
+
+            ranking_positions.append(current_dict)
+
+        score_matrix = []
+        for i in range(item_number):
+            score_matrix.append([])
+            for j in range(item_number):
+                if i == j: 
+                    score_matrix[i].append(0)
+                else: 
+                    balance = 0 
+
+                    for positions in ranking_positions:
+                        if positions.get(items[i], INF) < positions.get(items[j], INF):
+                            balance += 1
+                        else:
+                            balance -= 1
+                    
+                    if balance > 0:
+                        score_matrix[i].append(2)
+                    elif balance == 0:
+                        score_matrix[i].append(1)
+                    else:
+                        score_matrix[i].append(0)
+
+        item_scores = {}
+        for i in range(item_number):
+            score = 0 
+            for j in score_matrix[i]:
+                score += j
+
+            item_scores[items[i]] = score 
+
+        sorted_scores = sorted(item_scores.items(), key=lambda x:x[1], reverse=True)
+
+        final_ranking = [item for item, _ in sorted_scores]
+
+        return final_ranking[:len(rankings[0])]
+
+
+
